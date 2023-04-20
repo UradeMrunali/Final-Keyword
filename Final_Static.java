d7ad23de7626ccb1ac644b9ad739d92c1da9e2ae
Mrunali <mrunaliurade26@gmail.com>
2023-04-20 16:37:11 +0530

    example of Final Static

---
+++ Final_Static.java
@@ -0,0 +1,15 @@
+
+public class Final_Static {
+	//created static blank final variable
+ final static int a;
+ 
+ //initializing the value of a in static block
+ static
+ {
+	 a=99;
+ }
+ public static void main(String[] args) {
+	System.out.println("a:"+a);
+}
+	
+}
