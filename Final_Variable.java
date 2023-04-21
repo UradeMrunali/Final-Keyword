581e64005b990392663dadc69f90ce298cff7b6d
Mrunali <mrunaliurade26@gmail.com>
2023-04-21 22:31:04 +0530

    example of Final Variable

---
+++ Final_Variable.java
@@ -0,0 +1,13 @@
+
+public class Final_Variable {
+
+	public static void main(String[] args) {
+		//created final variable
+		final int age= 12;
+		
+		//trying to change value
+		//age=99;
+		
+		System.out.println("Age = "+age);
+	}
+}
