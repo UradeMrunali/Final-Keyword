fa378dac0c3ae9f62e5c539689eefe063d66bfce
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2023-04-21 22:28:59 +0530

    Update Final_Variable.java

--- Final_Variable.java
+++ Final_Variable.java
@@ -1,8 +1,8 @@
 
 public class Final_Variable {
 	public static void main(String[] args) {
-		//created final variable
-		final int age= 12;
+	//created final variable
+	final int age= 12;
 		
 		//trying to change value
 		//age=99;
